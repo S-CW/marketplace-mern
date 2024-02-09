1fd4263bdb643ae2ed2a2fa58f9cb47d2e0bbadd
Jeffrey <jeffreysing.jsl@gmail.com>
2024-02-09 19:29:28 +0800

    exclude user password data from api response

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -29,11 +29,12 @@ export const signin = async (req, res, next) =>
 
         // Auth session
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-        const {password: pass, ...rest} = validUser
+        // rest contain all user data exclude the password
+        const { password: pass, ...rest } = validUser._doc
         res.cookie('access_token', token, { httpOnly: true })
             .status(200)
             .json(rest);
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
