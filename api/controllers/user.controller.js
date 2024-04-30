05d35b9dfc1b7827c1d9a62a04f116df29e13cde
Jeffrey <jeffreysing1188@gmail.com>
2024-04-30 16:34:28 +0800

    add likes api, updated user schema and user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,16 +3,22 @@ import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import Listing from '../models/listing.model.js';
 
-export const test = (req, res) =>
-{
-    res.json({
-        message: 'Api route is working!'
-    });
-};
-
 
 export const updateUser = async (req, res, next) =>
 {
+    const { street, city, state, country } = req.body.contactInfo.address
+    const phoneNumber = req.body.contactInfo.phoneNumber
+
+    const updatedConnectInfo = {
+        phoneNumber: phoneNumber,
+        address: {
+            street: street,
+            city: city,
+            state: state,
+            country: country,
+        },
+    };
+
     // req.user is created from utils verifyToken
     if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account!'));
     try {
@@ -26,11 +32,13 @@ export const updateUser = async (req, res, next) =>
                 email: req.body.email,
                 password: req.body.password,
                 avatar: req.body.avatar,
+                connectInfo: updatedConnectInfo,
+                description: req.body.description,
             }
         }, { new: true });
 
         const { password, ...rest } = updateUser._doc;
-        res.status(200).json(rest);
+        return res.status(200).json(rest);
     } catch (error) {
         next(error);
     }
@@ -45,7 +53,7 @@ export const deleteUser = async (req, res, next) =>
         await User.findByIdAndDelete(req.params.id);
         await Listing.deleteMany({ userRef: req.params.id });
         res.clearCookie('access_token');
-        res.status(200).json('User has been deleted!');
+        return res.status(200).json('User has been deleted!');
     } catch (error) {
         next(error)
     }
@@ -57,7 +65,7 @@ export const getUserListings = async (req, res, next) =>
     if (req.user.id === req.params.id) {
         try {
             const listings = await Listing.find({ userRef: req.params.id });
-            res.status(200).json(listings);
+            return res.status(200).json(listings);
         } catch (error) {
             next(error)
         }
@@ -71,12 +79,37 @@ export const getUser = async (req, res, next) =>
 {
     try {
         const user = await User.findById(req.params.id);
-    
+
         if (!user) return next(errorHandler(404, 'User not found!'));
-    
+
         const { password: pass, ...rest } = user._doc;
-        res.status(200).json(rest);
+        return res.status(200).json(rest);
     } catch (error) {
         next(error);
+    }
+}
+
+
+export const updateLikes = async (req, res, next) =>
+{
+    if (req.user.id === req.params.id) return next(errorHandler(401, 'You cannot like your own profile'));
+
+    try {
+        const userId = req.user.id
+        const userLike = await User.findOne({ likes: userId })
+
+        if (userLike) {
+            userLike.likes.pull(userId)
+            userLike.save()
+            return res.status(200).json('Unlike profile!')
+        } else {
+            await User.findByIdAndUpdate(req.params.id, {
+                $push: { likes: userId }
+            });
+
+            return res.status(200).json('Like profile!')
+        }
+    } catch (error) {
+        next(error)
     }
 }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,18 @@
 import { timeStamp } from "console";
 import mongoose from "mongoose";
 
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String,
+    state: String,
+    country: String,
+});
+
+const contactInfoSchema = new mongoose.Schema({
+    phoneNumber: Number,
+    address: addressSchema,
+})
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -19,6 +31,15 @@ const userSchema = new mongoose.Schema({
     avatar: {
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    connectInfo: {
+        type: contactInfoSchema
+    },
+    description: {
+        type: String,
+    },
+    likes: {
+        type: [String],
     },
 }, { timestamps: true });
 
